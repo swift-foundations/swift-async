7adc5af06cffe3163d6716a14e96574ba646b528
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-12 20:15:08 +0100

    Convert dependencies to remote URLs

--- Package.swift
+++ Package.swift
@@ -22,9 +22,9 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../../swift-primitives/swift-async-primitives"),
-        .package(path: "../../swift-primitives/swift-buffer-primitives"),
-        .package(path: "../../swift-primitives/swift-test-primitives"),
+        .package(url: "https://github.com/swift-primitives/swift-async-primitives.git", from: "0.0.1"),
+        .package(url: "https://github.com/swift-primitives/swift-buffer-primitives.git", from: "0.0.1"),
+        .package(url: "https://github.com/swift-primitives/swift-test-primitives.git", from: "0.0.1"),
     ],
     targets: [
         .target(
